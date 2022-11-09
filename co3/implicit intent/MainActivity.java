cd160235ddfc385b0a2d525f44f4123a50cd4ac2
Manya M <93362285+Manya125@users.noreply.github.com>
2022-11-09 11:34:36 +0530

    Create MainActivity.java

---
+++ co3/implicit intent/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.implicit_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editText =findViewById(R.id.textView3);
+        Button btn =findViewById(R.id.button7);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
+
+
